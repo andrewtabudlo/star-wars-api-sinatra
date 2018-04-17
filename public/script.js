897b883d3c35db2641f72f6fd35812715566ea8e
andrewtabudlo <andrew.tabudlo@gmail.com>
2018-04-16 23:44:59 -0400

    add search functionality

--- public/script.js
+++ public/script.js
@@ -1,2 +1,3 @@
-var elem = document.querySelector('select');
-var instance = M.FormSelect.init(elem, options);
+$(document).ready(function(){
+    $('select').formSelect();
+});
--- server.rb
+++ server.rb
@@ -3,6 +3,63 @@ require 'sinatra/reloader'
 require 'swapir'
 
 get '/' do
-  "hello world"
   erb :index
+end
+
+post '/results/' do
+  params[:q]
+  category = params[:category]
+  case category
+  when "people"
+    @attributes = [
+      "height",
+      "mass",
+      "hair_color",
+      "skin_color",
+      "eye_color",
+      "birth_year",
+      "gender",
+      "homeworld",
+      "films",
+      "species",
+      "vehicles",
+      "starships",
+      "created",
+      "edited",
+      "url"
+    ]
+    @result = Swapir.search_people(params[:q])
+  when "films"
+    @attributes = [
+      "title",
+      "episode_id",
+      "director",
+      "producer"
+    ]
+    @result = Swapir.search_films(params[:q])
+  when "planets"
+    @result = Swapir.search_planets(params[:q])
+  when "species"
+    @result = Swapir.search_species(params[:q])
+  when "starships"
+    @attributes = [
+      "name",
+      "model",
+      "manufacturer",
+      "cost_in_credits",
+      "length",
+      "max_atmosphering_speed",
+      "crew",
+      "passengers",
+      "cargo_capacity",
+      "consumeables",
+      "hyperdrive_rating"
+    ]
+    @result = Swapir.search_starships(params[:q])
+  when "vehicles"
+    @result = Swapir.search_vehicles(params[:q])
+  end
+
+  puts @result
+  erb :results
 end
